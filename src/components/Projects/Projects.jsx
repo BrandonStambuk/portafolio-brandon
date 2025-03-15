7d028a6b1087b04ddf5aba806a69a4f1945dc825
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2025-03-15 14:34:05 -0400

    add: hourglass loader

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,23 @@
+import styles from "./Projects.module.css";
 
-import styles from './Projects.module.css'
- export const Projects = () => {
-    return (
-        <div id='Projects' className={styles.containerProjects}>
-            <h1 className={styles.title}>My Projects</h1>
-            <h2 className={styles.subtitle}>Nothing  (yet) </h2>
+export const Projects = () => {
+  return (
+    <div id="Projects" className={styles.containerProjects}>
+      <h1 className={styles.title}>My Projects</h1>
+      <div className={styles.hourglassBackground}>
+        <div className={styles.hourglassContainer}>
+          <div className={styles.hourglassCurves}></div>
+          <div className={styles.hourglassCapTop}></div>
+          <div className={styles.hourglassGlassTop}></div>
+          <div className={styles.hourglassSand}></div>
+          <div className={styles.hourglassSandStream}></div>
+          <div className={styles.hourglassCapBottom}></div>
+          <div className={styles.hourglassGlass}></div>
+          
         </div>
-    );
+       
+      </div>
+      <h2 className={styles.subtitle}>Loading...</h2>
+    </div>
+  );
 };
-
